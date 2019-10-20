1dd868f577f93a7f85e125dfde219680d280de49
Jackson Foster <jacksonfoster444@gmail.com>
2019-10-19 18:52:47 -0700

    initialize repo

---
+++ back-end/payroll-pal-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jacksonfoster/Code/projects/payroll-pal/back-end/payroll-pal-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
