5f83909eb0a62a66ad10100472cb4f58e13fc29a
Jackson Foster <jacksonfoster444@gmail.com>
2019-11-23 13:13:41 -0800

    added build

---
+++ front-end/payroll-pal/build/precache-manifest.feff73339129c046f86b46292a16fc4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0688f978ae4ea975cfb152986c26cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161911141b9a1cb72db2",
+    "url": "/static/css/main.4d2da84d.chunk.css"
+  },
+  {
+    "revision": "7dcf43d8b4fe2e08e320",
+    "url": "/static/js/2.f2b24ef1.chunk.js"
+  },
+  {
+    "revision": "161911141b9a1cb72db2",
+    "url": "/static/js/main.9c1bfb31.chunk.js"
+  },
+  {
+    "revision": "efb176934ca2743788c4",
+    "url": "/static/js/runtime-main.5ffed5d0.js"
+  },
+  {
+    "revision": "bdaaaccabd8b44a5488994c491cf896b",
+    "url": "/static/media/Payroll-Pal-Logo Large.bdaaacca.svg"
+  },
+  {
+    "revision": "245426adb5fde4025f4ea9fe507bbc3d",
+    "url": "/static/media/Payroll-Pal-Logo Small.245426ad.svg"
+  }
+]);
